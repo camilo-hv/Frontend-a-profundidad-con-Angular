1902e0e788d76b37c0ed269107e8b876f74890eb
Camilo Henao <camivallejo2010@hotmail.com>
2025-02-22 21:33:47 -0500

    index

---
+++ Curso-de-Closures-y-Scope-en-JavaScript/src/hoisting/index.js
@@ -0,0 +1,25 @@
+// var nameOfDog; //undefined
+// console.log(nameOfDog);
+// var nameOfDog = 'Tobby';
+// console.log(nameOfDog);
+
+var tobby; //undefined
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${tobby}`);
+}
+var tobby = 'Tobby';
+
+
+
+
+//Debugger
+var a = 'Hello';
+ function hello(){
+    let b = 'Hello World';
+    const c = 'Hello World!';
+    debugger;
+}
+
+hello();
